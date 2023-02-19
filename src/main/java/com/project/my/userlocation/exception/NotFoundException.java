a71cb15773595c7a9f1cd0f51ca9e42e11d51eca
Mohsen Mirzaei <mohsen.mirzaei@dfreight.org>
2023-02-20 00:26:03 +0330

    added new exception class for not found errors.

---
+++ src/main/java/com/project/my/userlocation/exception/NotFoundException.java
@@ -0,0 +1,9 @@
+package com.project.my.userlocation.exception;
+
+public class NotFoundException extends RuntimeException {
+
+    public NotFoundException(String message) {
+        super(message);
+    }
+
+}
