a51b26f5cad0a8877dfc8d5070082d949d9c88c0
Mohsen Mirzaei <mohsen.mirzaei@dfreight.org>
2023-02-19 23:17:41 +0330

    initial commit.

---
+++ src/main/java/com/project/my/userlocation/SimpleUserLocationApplication.java
@@ -0,0 +1,13 @@
+package com.project.my.userlocation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleUserLocationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleUserLocationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/my/userlocation/SimpleUserLocationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.my.userlocation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleUserLocationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
