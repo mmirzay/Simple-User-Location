274604f14e69189d0132a1d43e0db26d97dd6355
Mohsen Mirzaei <mohsen.mirzaei@dfreight.org>
2023-02-20 23:44:17 +0330

    added new exception class for date formatting exceptions.

---
+++ src/main/java/com/project/my/userlocation/exception/DateFormatException.java
@@ -0,0 +1,9 @@
+package com.project.my.userlocation.exception;
+
+public class DateFormatException extends RuntimeException {
+
+    public DateFormatException(String message) {
+        super(message);
+    }
+
+}
